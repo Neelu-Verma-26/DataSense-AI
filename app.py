064a795166c531c3f1d3b9c2ae55e174608276e2
Neelu-Verma-26 <vermaneelu2003@gmail.com>
2026-07-14 01:08:09 +0530

    Implement dataset upload feature

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+print("Running DataSense AI")
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
+    return "THIS IS DATASENSE AI"
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        uploaded_file = request.files["file"]
+        uploaded_file.save("uploads/" + uploaded_file.filename)
+        return "File Uploaded Successfully!"
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, port=5001)
