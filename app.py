b641558099edc52c52c681080256a650a340dfc9
Neelu-Verma-26 <vermaneelu2003@gmail.com>
2026-07-17 01:27:58 +0530

    Add duplicate removal feature using Flask sessions

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import pandas as pd
 
 app = Flask(__name__)
+app.secret_key =  "datasense_ai_secret_key"
 
 @app.route("/")
 def welcome():
@@ -13,6 +14,8 @@ def upload():
         uploaded_file = request.files["file"]
         file_path = "uploads/" + uploaded_file.filename
 
+        session["file_path"] = file_path
+
         uploaded_file.save(file_path)
         df = pd.read_csv(file_path)
 
@@ -20,6 +23,8 @@ def upload():
         missing_values = missing_values[missing_values > 0]
         if missing_values.empty:
             missing_values_html = "<p>No Missing Values found</p>"
+        else:
+            missing_values_html = (missing_values.to_frame(name="Missing Values").to_html())
 
         duplicate_rows = df.duplicated().sum()
     
@@ -38,6 +43,38 @@ def upload():
     
     return render_template("index.html")
 
+@app.route("/remove_duplicates", methods = ["POST"])
+def remove_duplicates():
+    file_path = session.get("file_path")
+    
+    df = pd.read_csv(file_path)
+
+    df.drop_duplicates(inplace=True)
+
+    df.to_csv(file_path, index=False)
+
+    rows = df.shape[0]
+    columns = df.shape[1]
+    column_names = list(df.columns)
+
+    missing_values = df.isnull().sum()
+    missing_values = missing_values[missing_values > 0]
+    if missing_values.empty:
+        missing_values_html = "<p>No Missing Values found</p>"
+    else:
+        missing_values_html = (missing_values.to_frame(name="Missing Values").to_html())
+
+    duplicate_rows = df.duplicated().sum()
+
+    table = df.head().to_html()
+
+    return render_template("index.html",
+                                table=table,
+                                rows=rows,
+                                columns=columns,
+                                column_names=column_names,
+                                missing_values = missing_values_html,
+                                duplicate_rows = duplicate_rows)
 
 if __name__ == "__main__":
     app.run(debug = True, port=5001)
